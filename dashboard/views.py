874850cf4fa51a3cbc941dc9af3ebb1427dc8696
Ravi Agrawal <raviagrawal1012@gmail.com>
2020-09-07 16:08:31 +0530

    Dynamic Map Values

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 import pandas as pd
 
 # Create your views here.
+df3=pd.read_json('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json')
+
 def index(request):
     url="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
     confirmedGlobal=pd.read_csv(url,error_bad_lines=False)
@@ -12,5 +14,21 @@ def index(request):
     barPlotData=barPlotData.sort_values(by='values',ascending=False)
     barPlotVals=barPlotData['values'].values.tolist()
     countryNames=barPlotData['Country/Region'].values.tolist()
-    context={'totalCount':totalCount,'countryNames' : countryNames, 'barPlotVals' : barPlotVals}
+    dataForMap=mapDataCal(barPlotData,countryNames)
+    context={'totalCount':totalCount,'countryNames' : countryNames, 'barPlotVals' : barPlotVals,'dataForMap' : dataForMap}
     return render(request,'index.html',context)
+
+def mapDataCal(barPlotData,countryNames):
+    dataForMap=[]
+    for i in countryNames:
+        try:
+            tempdf=df3[df3['name']==i]
+            temp={}
+            temp["code3"]=list(tempdf['code3'].values)[0]
+            temp["name"]=i
+            temp["value"]=barPlotData[barPlotData['Country/Region']==i]['values'].sum()
+            temp["code"]=list(tempdf['code'].values)[0]
+            dataForMap.append(temp)
+        except:
+            pass
+    return dataForMap
