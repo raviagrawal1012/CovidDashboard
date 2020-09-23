276528399e13eefab91bbc7c3b41ded5e0363242
Ravi Agrawal <raviagrawal1012@gmail.com>
2020-09-23 21:54:55 +0530

    Dynamic India Map Data

--- dashboard/views.py
+++ dashboard/views.py
@@ -35,14 +35,50 @@ def mapDataCal(barPlotData,countryNames):
     return dataForMap
 
 def india(request):
-    latestIndList = IndTable()
-    context = {'latestIndList' : latestIndList}
+    latestIndList,latestInd = IndTable()
+    lst = indiaValue(latestInd)
+    context = {'latestIndList' : latestIndList,'lst':lst}
     return render(request,'India.html',context)
 
 def IndTable():
     india = pd.read_csv("https://api.covid19india.org/csv/latest/states.csv")
     latestInd = india[-36:]
     latestInd=latestInd.sort_values(by='Confirmed',ascending=False)
-    
     latestIndList = latestInd.values.tolist()
-    return latestIndList
+    return latestIndList,latestInd
+
+def indiaValue(latestInd):
+    indiaList =latestInd['State'].values.tolist()
+    indiaConList = latestInd['Confirmed'].values.tolist()
+    indiaDict = {'in-py':'Puducherry','in-wb':'West Bengal','in-or':'Odisha',
+            'in-br':'Bihar','in-sk':'Sikkim','in-ct':'Chhattisgarh',
+            'in-tn':'Tamil Nadu','in-mp':'Madhya Pradesh',
+            'in-2984':'Gujarat','in-ga':'Goa','in-nl':'Nagaland',
+            'in-mn':'Manipur','in-ar':'Arunachal Pradesh',
+            'in-mz':'Mizoram','in-tr':'Tripura',
+            'in-3464':'Dadra and Nagar Haveli and Daman and Diu',
+            'in-dl':'Delhi','in-hr':'Haryana','in-ch':'Chandigarh',
+            'in-hp':'Himachal Pradesh','in-jk':'Jammu and Kashmir',
+            'in-kl':'Kerala','in-ka':'Karnataka',
+            'in-dn':'Dadra and Nagar Haveli and Daman and Diu',
+            'in-mh':'Maharashtra','in-as':'Assam','in-ap':'Andhra Pradesh',
+            'in-ml':'Meghalaya','in-pb':'Punjab','in-rj':'Rajasthan',
+            'in-up':'Uttar Pradesh','in-ut':'Uttarakhand',
+            'in-jh':'Jharkhand'}
+
+    stateConDict = {indiaList[i] : indiaConList[i] for i in range(len(indiaList))}
+    finalMapDict = {}
+    for i in indiaDict:
+        for j in stateConDict:
+            if indiaDict[i] == j:
+                finalMapDict[i] = stateConDict[j]
+
+    lst = []
+    for i in finalMapDict:
+        lst.append([i,finalMapDict[i]])
+
+    return lst
+
+
+
+
